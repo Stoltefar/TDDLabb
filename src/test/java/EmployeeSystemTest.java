7498f6b21a4c0d9b9093f55c933eec622b473737
Joakim <joakim.sorkka@iths.se>
2022-02-14 10:43:10 +0100

    Green

--- src/test/java/EmployeeSystemTest.java
+++ src/test/java/EmployeeSystemTest.java
@@ -6,8 +6,8 @@ public class EmployeeSystemTest {
     @Test
     public void createEmployeeSystem() {
         EmployeeSystem testSystem;
+        testSystem = new EmployeeSystem();
 
         assertInstanceOf(EmployeeSystem.class,testSystem);
-
     }
 }
