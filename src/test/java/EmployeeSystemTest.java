aaa02602b9005d1552c6b3bec686da046c324aa3
Joakim <joakim.sorkka@iths.se>
2022-02-14 10:41:18 +0100

    Red

--- src/test/java/EmployeeSystemTest.java
+++ src/test/java/EmployeeSystemTest.java
@@ -1,9 +1,13 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class EmployeeSystemTest {
 
     @Test
     public void createEmployeeSystem() {
         EmployeeSystem testSystem;
+
+        assertInstanceOf(EmployeeSystem.class,testSystem);
+
     }
 }
