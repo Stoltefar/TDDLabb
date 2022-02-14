7ac99713aa80e18321eb0bea672a01dce55c3baf
Joakim <joakim.sorkka@iths.se>
2022-02-14 12:41:40 +0100

    RED

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,9 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
 
     Employee employeeObject;
 
+    @BeforeEach
+    public void setUpEmployee() {
+        employeeObject = new Employee();
+    }
+
+    @Test
+    public void createEmployee() {
+        employeeObject = new Employee();
+        assertInstanceOf(Employee.class,employeeObject);
+    }
+
+    @Test
+    public void checkNameVariable() {
+        //Arrange
+        employeeObject.setName("Adam");
+    }
 
 
 }
