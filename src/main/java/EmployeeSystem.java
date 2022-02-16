73a783ae46c7c2d089a53b9d993448ec9bc93078
Joakim <joakim.sorkka@iths.se>
2022-02-16 10:37:11 +0100

    Green

--- src/main/java/EmployeeSystem.java
+++ src/main/java/EmployeeSystem.java
@@ -8,4 +8,13 @@ public class EmployeeSystem {
     public void addNewEmployee(Employee newEmployee) {
         employeeList.add(newEmployee);
     }
+
+    public void removeEmployee(int i) {
+        for(int j = 0; j < employeeList.size();j++) {
+            if(employeeList.get(j).getId() == i) {
+                employeeList.remove(j);
+                break;
+            }
+        }
+    }
 }
