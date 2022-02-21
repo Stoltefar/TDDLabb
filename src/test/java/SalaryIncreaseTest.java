ca74c235683161d037fceffac59e77d89df2334a
Linus Callheim <stoltefar@gmail.com>
2022-02-21 10:36:34 +0100

    GREEN

--- src/test/java/SalaryIncreaseTest.java
+++ src/test/java/SalaryIncreaseTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SalaryIncreaseTest {
     EmployeeSystem testSystem;
 
