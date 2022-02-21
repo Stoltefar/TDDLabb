6f9f08972b912019c838ff7fb6d3b25af09e8213
Linus Callheim <stoltefar@gmail.com>
2022-02-21 11:50:43 +0100

    GREEN

--- src/main/java/EmployeeSystem.java
+++ src/main/java/EmployeeSystem.java
@@ -19,7 +19,7 @@ public class EmployeeSystem {
     }
 
     public void universalRaise(double raisePercentage) {
-        if (raisePercentage >= 0) {
+        if (raisePercentage >= 0 && raisePercentage<=100) {
             double raiseFactor = 1 + (raisePercentage / 100);
             for (int i = 0; i < employeeList.size(); ++i) {
                 employeeList.get(i).setSalary(raiseFactor * employeeList.get(i).getSalary());
